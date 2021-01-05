65177650b8905f1e8c7f5c9d5edb59110eb2682f
awanz <hermawan9815@gmail.com>
2021-01-05 12:31:16 +0700

    update to tours

---
+++ README.md
@@ -0,0 +1,12 @@
+# Go API Basic with struct only
+
+## Deploy to Heroku
+`git init` initialization git repository
+
+`heroku create -b https://github.com/heroku/heroku-buildpack-go.git` create heroku and add buildpack go lang
+
+`git add .` add all file to stage
+
+`git commit -m "your massage"` make tag or massage to your file
+
+`git push heroku master` add push to heroku server
--- main.go
+++ main.go
@@ -3,114 +3,133 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
+type tour struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Location    string `json:"location"`
 }
 
-type allEvents []event
+type allTours []tour
 
-var events = allEvents{
+var tours = allTours{
 	{
 		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+		Title:       "Pantai Pangandaran",
+		Description: "Pantai Pangandaran pernah dinobatkan oleh AsiaRooms sebagai Pantai terbaik di provinsi Jawa Barat.",
+		Location:    "Kabupaten Pangandaran, Jawa Barat",
 	},
+	{
+		ID:          "2",
+		Title:       "Pantai Karang Tawulan",
+		Description: "Pantai Karang Tawulan yang indah ini terletak di Desa Cimanuk, Kalapagenep, Kecamatan Cikalong, Tasikmalaya.",
+		Location:    "Kabupaten Tasikmalaya, Jawa Barat",
+	},
+	{
+		ID:          "3",
+		Title:       "Karaha Bodas",
+		Description: "Karaha Bodas merupakan kawah gunung api muda yang terletak di Desa Kadipaten, Kecamatan Kadipaten, Kabupaten Tasikmalaya.",
+		Location:    "Kabupaten Pangandaran, Jawa Barat",
+	},
+}
+
+func getAllTours(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(tours)
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to GOAPI!")
 }
 
-func createEvent(w http.ResponseWriter, r *http.Request) {
-	var newEvent event
-	// Convert r.Body into a readable formart
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event id, title and description only in order to update")
-	}
+// func createTour(w http.ResponseWriter, r *http.Request) {
+// 	var newTour tour
+// 	// Convert r.Body into a readable formart
+// 	reqBody, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Kindly enter data with the event id, title and description only in order to update")
+// 	}
 
-	json.Unmarshal(reqBody, &newEvent)
+// 	json.Unmarshal(reqBody, &newTour)
 
-	// Add the newly created event to the array of events
-	events = append(events, newEvent)
+// 	// Add the newly created event to the array of events
+// 	tours = append(tours, newTour)
 
-	// Return the 201 created status code
-	w.WriteHeader(http.StatusCreated)
-	// Return the newly created event
-	json.NewEncoder(w).Encode(newEvent)
-}
+// 	// Return the 201 created status code
+// 	w.WriteHeader(http.StatusCreated)
+// 	// Return the newly created event
+// 	json.NewEncoder(w).Encode(newTour)
+// }
 
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	// Get the ID from the url
-	eventID := mux.Vars(r)["id"]
+// func getOneEvent(w http.ResponseWriter, r *http.Request) {
+// 	// Get the ID from the url
+// 	eventID := mux.Vars(r)["id"]
 
-	// Get the details from an existing event
-	// Use the blank identifier to avoid creating a value that will not be used
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
+// 	// Get the details from an existing event
+// 	// Use the blank identifier to avoid creating a value that will not be used
+// 	for _, singleEvent := range events {
+// 		if singleEvent.ID == eventID {
+// 			json.NewEncoder(w).Encode(singleEvent)
+// 		}
+// 	}
+// }
 
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(events)
-}
+// func getAllEvents(w http.ResponseWriter, r *http.Request) {
+// 	json.NewEncoder(w).Encode(events)
+// }
 
-func updateEvent(w http.ResponseWriter, r *http.Request) {
-	// Get the ID from the url
-	eventID := mux.Vars(r)["id"]
-	var updatedEvent event
-	// Convert r.Body into a readable formart
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
+// func updateEvent(w http.ResponseWriter, r *http.Request) {
+// 	// Get the ID from the url
+// 	eventID := mux.Vars(r)["id"]
+// 	var updatedEvent event
+// 	// Convert r.Body into a readable formart
+// 	reqBody, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+// 	}
 
-	json.Unmarshal(reqBody, &updatedEvent)
+// 	json.Unmarshal(reqBody, &updatedEvent)
 
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			singleEvent.Title = updatedEvent.Title
-			singleEvent.Description = updatedEvent.Description
-			events[i] = singleEvent
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
+// 	for i, singleEvent := range events {
+// 		if singleEvent.ID == eventID {
+// 			singleEvent.Title = updatedEvent.Title
+// 			singleEvent.Description = updatedEvent.Description
+// 			events[i] = singleEvent
+// 			json.NewEncoder(w).Encode(singleEvent)
+// 		}
+// 	}
+// }
 
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	// Get the ID from the url
-	eventID := mux.Vars(r)["id"]
+// func deleteEvent(w http.ResponseWriter, r *http.Request) {
+// 	// Get the ID from the url
+// 	eventID := mux.Vars(r)["id"]
 
-	// Get the details from an existing event
-	// Use the blank identifier to avoid creating a value that will not be used
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
-		}
-	}
-}
+// 	// Get the details from an existing event
+// 	// Use the blank identifier to avoid creating a value that will not be used
+// 	for i, singleEvent := range events {
+// 		if singleEvent.ID == eventID {
+// 			events = append(events[:i], events[i+1:]...)
+// 			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+// 		}
+// 	}
+// }
 
 func main() {
-	port := os.Getenv("PORT")
+	/* enable if you want deploy */
+	// port := os.Getenv("PORT")
+	/* enable when localhost */
+	port := "80"
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
-	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/tours", getAllTours).Methods("GET")
+	// router.HandleFunc("/event", createEvent).Methods("POST")
+	// router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	// router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	// router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
